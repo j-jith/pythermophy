Fixed the partial derivative dp_dT_v in Lee-Kesler

compare/gen_data.py
@@ -12,17 +12,24 @@ T_0 = np.array([40, 60, 80, 100]) + 273.15 # [K]
 ### Mean pressure of fluid
 p_0 = np.linspace(0.1e6, 20e6, 50) # [Pa]
 
-# CO2 properties
-M = 44.01e-3 # kg/mol
-pc = 7.38e6 # Pa
-Tc = 31.1 + 273.15 # K
-omega = 0.228 # acentric factor
+# # CO2 properties
+# M = 44.01e-3 # kg/mol
+# pc = 7.38e6 # Pa
+# Tc = 31.1 + 273.15 # K
+# omega = 0.228 # acentric factor
+# 
+# ig = eos.IdealGas(M, 'CO2')
+# rk = eos.RK(Tc, pc, M, 'CO2')
+# srk = eos.SRK(Tc, pc, M, omega, 'CO2')
+# pr = eos.PR(Tc, pc, M, omega, 'CO2')
+# lk = eos.LK(Tc, pc, M, omega, 'CO2')
 
-ig = eos.IdealGas(M, 'CO2')
-rk = eos.RK(Tc, pc, M, 'CO2')
-srk = eos.SRK(Tc, pc, M, omega, 'CO2')
-pr = eos.PR(Tc, pc, M, omega, 'CO2')
-lk = eos.LK(Tc, pc, M, omega, 'CO2')
+fluid = eos.Fluid.init_from_file('../fluids/CO2')
+ig = eos.IdealGas(fluid)
+rk = eos.RK(fluid)
+srk = eos.SRK(fluid)
+pr = eos.PR(fluid)
+lk = eos.LK(fluid)
 
 eqs = [ig, rk, srk, pr, lk]
 eqs_labels = ['Ideal gas', 'RK', 'SRK', 'PR', 'LK']

compare/plot.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 from customize_matplotlib import *
 customize_matplotlib()
 
-#T = 40.0
-# = 60.0
-#T = 80.0
-T = 100.0
-
+T = 40.0
 show_legend = True
+# show_legend = False
+# T = 60.0
+# T = 80.0
+# T = 100.0
+
 
 rho = np.loadtxt('data/rho_{}.txt'.format(T))
 c = np.loadtxt('data/c_{}.txt'.format(T))

eos/lee_kesler.py
@@ -1,7 +1,7 @@
 from __future__ import division, print_function
 import numpy as np
 import scipy.optimize as spo
-from math import exp, log
+from math import exp
 
 from parent_class import EOS
 
@@ -48,7 +48,37 @@ class LeeKesler(EOS):
         self.p_crit = fluid.p_crit # Pa
         self.T_crit = fluid.T_crit # K
 
-    def get_reduced_volume(self, Tr, pr, fluid='simple'):
+    def get_B(self, Tr, fluid):
+        if fluid=='reference':
+            i = 1
+        elif fluid=='simple':
+            i = 0
+        else:
+            return None
+
+        return self.b1[i] - self.b2[i]/Tr - self.b3[i]/Tr**2 - self.b4[i]/Tr**3
+
+    def get_C(self, Tr, fluid):
+        if fluid=='reference':
+            i = 1
+        elif fluid=='simple':
+            i = 0
+        else:
+            return None
+
+        return self.c1[i] - self.c2[i]/Tr + self.c3[i]/Tr**3
+
+    def get_D(self, Tr, fluid):
+        if fluid=='reference':
+            i = 1
+        elif fluid=='simple':
+            i = 0
+        else:
+            return None
+
+        return self.d1[i] + self.d2[i]/Tr
+
+    def get_reduced_volume(self, Tr, pr, fluid, **kwargs):
 
         if fluid=='reference':
             i = 1
@@ -57,18 +87,26 @@ class LeeKesler(EOS):
         else:
             return None
 
-        B = self.b1[i] - self.b2[i]/Tr - self.b3[i]/Tr**2 - self.b4[i]/Tr**3
-        C = self.c1[i] - self.c2[i]/Tr + self.c3[i]/Tr**3
-        D = self.d1[i] + self.d2[i]/Tr
+        B = self.get_B(Tr, fluid)
+        C = self.get_C(Tr, fluid)
+        D = self.get_D(Tr, fluid)
 
         def objfun(x):
             return -pr*x/Tr + 1 + B/x + C/x**2 + D/x**5 + self.c4[i]/Tr**3/x**2 * (self.beta[i] + self.gamma[i]/x**2) * exp(-self.gamma[i]/x**2)
 
         # initial gues for nonlinear solver
-        if pr<1:
-            init_vol = 10.
+        if 'init_vol' in kwargs:
+            init_vol = kwargs['init_vol']
         else:
-            init_vol = 0.1
+            init_vol = Tr/pr
+            #if pr<1:
+            #    init_vol = 10.
+            #else:
+            #    init_vol = 0.1
+            #if Tr/pr > 1:
+            #    init_vol = 10.
+            #else:
+            #    init_vol = Tr/pr
 
         #vol, info, ier, mesg = fsolve(objfun, init_vol)
         result = spo.root(objfun, init_vol, method='lm')
@@ -79,13 +117,21 @@ class LeeKesler(EOS):
         else:
             return result.x
 
-    def get_Z(self, T, p):
+
+    def get_Z(self, T, p, **kwargs):
         """
         Get the compressibility factor for a real gas
+
         Parameters
         ----------
         T - Temperature [K]
         p - Pressure [Pa]
+
+        Optional parameters
+        -------------------
+        init_vol - Initial value for the non-linear solution of
+        the Lee-Kesler equation. Should be a list [v0, vr] of
+        "reduced" volumes of the simple and reference fluids, respectively.
 
         Returns
         -------
@@ -95,22 +141,26 @@ class LeeKesler(EOS):
         Tr = T/self.T_crit
         pr = p/self.p_crit
 
-        z0 = pr/Tr * self.get_reduced_volume(Tr, pr, 'simple')
-        zr = pr/Tr * self.get_reduced_volume(Tr, pr, 'reference')
+        if 'init_vol' in kwargs:
+            init_vol = kwargs['init_vol']
+        else:
+            init_vol = [Tr/pr, Tr/pr]
+
+        z0 = pr/Tr * self.get_reduced_volume(Tr, pr, 'simple', init_vol=init_vol[0])
+        zr = pr/Tr * self.get_reduced_volume(Tr, pr, 'reference', init_vol=init_vol[1])
 
         # departure term
         z1 = (zr - z0)/self.acentric_r
 
         z = z0 + self.acentric*z1
 
-        #print('z0 = ', z0)
-        #print('zr = ', zr)
-        #print('z1 = ', z1)
-        #print('z = ', z)
+        #print('z0 =', z0)
+        #print('z1 =', z1)
 
         return z
 
-    def get_reduced_departure_energy(self, Tr, pr, fluid='simple', **kwargs):
+
+    def get_departure_cv_aux(self, Tr, pr, fluid, **kwargs):
         if fluid=='reference':
             i = 1
         elif fluid=='simple':
@@ -123,73 +173,15 @@ class LeeKesler(EOS):
         else:
             vr = self.get_reduced_volume(Tr, pr, fluid)
 
-        du = Tr*(-1/Tr/vr * (self.b2[i] + 2*self.b3[i]/Tr + self.b4[i]/Tr**2) \
-                -1/2/Tr/vr**2 * (self.c2[i] - 3*self.c3[i]/Tr**2) \
-                + self.d2[i]/5/Tr/vr**5 \
-                + 3*self.c4[i]/2/Tr**3/self.gamma[i] * \
-                (self.beta[i] + 1 - (self.beta[i] + 1 + self.gamma[i]/vr**2)*exp(-self.gamma[i]/vr**2)))
+        E = self.c4[i]/2/Tr**3/self.gamma[i] * ( self.beta[i] + 1 - (self.beta[i] + 1 + self.gamma[i]/vr**2)*exp(-self.gamma[i]/vr**2) )
 
-        return du
+        d_cv = 2*(self.b3[i] + 3*self.b4[i]/Tr)/Tr**2/vr - 3*self.c3[i]/Tr**3/vr**2 - 6*E
 
+        return d_cv
 
-    def get_departure_energy(self, T, p, **kwargs):
-
-        Tr = T/self.T_crit
-        pr = p/self.p_crit
-
-        if 'Vr' in kwargs:
-            vr = kwargs['Vr']
-            du0 = self.R*self.T_crit * self.get_reduced_departure_energy(Tr, pr, 'simple', Vr=vr[0])
-            dur = self.R*self.T_crit * self.get_reduced_departure_energy(Tr, pr, 'reference', Vr=vr[1])
-        else:
-            du0 = self.R*self.T_crit * self.get_reduced_departure_energy(Tr, pr, 'simple')
-            dur = self.R*self.T_crit * self.get_reduced_departure_energy(Tr, pr, 'reference')
-
-        # departure term
-        du1 = (dur - du0)/self.acentric_r
-
-        return du0 + self.acentric*du1
-
-
-    def get_departure_enthalpy(self, T, p, **kwargs):
-
-        u = self.get_departure_energy(T, p)
-
-        if 'Z' in kwargs:
-            z = kwargs['Z']
-        else:
-            z = self.get_Z(T, p)
-
-        return self.R*T*(z-1) + u
-
-    def get_departure_cp(self, T, p, step=1e-3, **kwargs):
+    def get_departure_cv(self, T, p):
         """
-        Get the departure (difference between real gas and ideal gas) for isobaric specific heat capacity (C_p) [J/mol/K]
-
-        Parameters
-        ----------
-        T    - Temperature [K]
-        p    - Pressure [Pa]
-        step - Temperature step size for numerical differentiation [K]
-
-        Optional parameters
-        -------------------
-        Z - Compressibility factor [dimensionless]
-        This function recalculates compressibility factor if it is not given as an optional parameter
-
-        Returns
-        -------
-        Departure for isobaric specific heat capacity [J/mol/K]
-        """
-
-        h2 = self.get_departure_enthalpy(T+step, p, **kwargs)
-        h1 = self.get_departure_enthalpy(T-step, p, **kwargs)
-
-        return (h2 - h1)/2/step
-
-    def get_departure_cv(self, T, p, step=1e-3, **kwargs):
-        """
-        Get the departure (difference between real gas and ideal gas) for isochoric specific heat capacity (C_p) [J/mol/K]
+        Get the departure (difference between real gas and ideal gas) for isochoric specific heat capacity (C_v) [J/mol/K]
 
         Parameters
         ----------
@@ -200,24 +192,57 @@ class LeeKesler(EOS):
         -------
         Departure for isochoric specific heat capacity [J/mol/K]
         """
+
         Tr = T/self.T_crit
         pr = p/self.p_crit
 
-        vr0 = self.get_reduced_volume(Tr, pr, 'simple')
-        vrr = self.get_reduced_volume(Tr, pr, 'reference')
+        cv_0 = self.get_departure_cv_aux(Tr, pr, 'simple')
+        cv_r = self.get_departure_cv_aux(Tr, pr, 'reference')
 
-        u2 = self.get_departure_energy(T+step, p, Vr=[vr0, vrr])
-        u1 = self.get_departure_energy(T-step, p, Vr=[vr0, vrr])
+        cv_1 = (cv_r - cv_0)/self.acentric_r
 
-        return (u2 - u1)/2/step
+        return self.R * (cv_0 + self.acentric*cv_1)
 
-    def get_pdiff_Z_p_T(self, T, p, step=1e-3):
-        z2 = self.get_Z(T, p+step)
-        z1 = self.get_Z(T, p-step)
-        return (z2 - z1)/2/step
+
+
+    def get_pdiff_pr_Tr_Vr(self, Tr, Vr, fluid):
+        if fluid=='reference':
+            i = 1
+        elif fluid=='simple':
+            i = 0
+        else:
+            return None
+
+        # pdiff = 1/Vr * ( 1 + (self.b1[i] + self.b3[i]/Tr**2 + 2*self.b4[i]/Tr**3)/Vr
+        #                  + (self.c1[i] - 2*self.c3[i]/Tr**3)/2/Vr**2 + self.d1[i]/5/Vr**5
+        #                  - 2*self.c4[i]/Tr**3/Vr**2 * (self.beta[i] + self.gamma[i]/Vr**2) * exp(-self.gamma[i]/Vr**2) )
+        pdiff = 1/Vr * ( 1 + (self.b1[i] + self.b3[i]/Tr**2 + 2*self.b4[i]/Tr**3)/Vr
+                         + (self.c1[i] - 2*self.c3[i]/Tr**3)/Vr**2 + self.d1[i]/Vr**5
+                         - 2*self.c4[i]/Tr**3/Vr**2 * (self.beta[i] + self.gamma[i]/Vr**2) * exp(-self.gamma[i]/Vr**2) )
+
+        return pdiff
+
+    def get_pdiff_pr_Vr_Tr(self, Tr, Vr, fluid):
+        if fluid=='reference':
+            i = 1
+        elif fluid=='simple':
+            i = 0
+        else:
+            return None
+
+        B = self.get_B(Tr, fluid)
+        C = self.get_C(Tr, fluid)
+        D = self.get_D(Tr, fluid)
+
+        pdiff = -Tr/Vr**2 * ( 1 + 2*B/Vr + 3*C/Vr**2 + 6*D/Vr**5
+                              + self.c4[i]/Tr**3/Vr**2
+                              * (3*self.beta[i] + (5-2*self.beta[i]-2*self.gamma[i]/Vr**2)*self.gamma[i]/Vr**2)
+                              * exp(-self.gamma[i]/Vr**2) )
+
+        return pdiff
 
     # Isothermal compressibililty
-    def get_isothermal_compressibility(self, T, p, **kwargs):
+    def get_isothermal_compressibility(self, T, p):
         """
         Get the isothermal compressibility of a real gas
 
@@ -226,21 +251,94 @@ class LeeKesler(EOS):
         T - Temperature [K]
         p - Pressure [Pa]
 
-        Optional parameters
-        -------------------
-        Z - Compressibility factor [dimensionless]
-        This function recalculates compressibility factor if it is not given as an optional parameter
-
         Returns
         -------
         Isothermal compressibility [1/Pa]
         """
 
-        if 'Z' in kwargs:
-            Z = kwargs['Z']
-        else:
-            Z = self.get_Z(T, p)
+        Tr = T/self.T_crit
+        pr = p/self.p_crit
 
-        Z1 = self.get_pdiff_Z_p_T(T, p)
+        fluids = ['simple', 'reference']
 
-        return 1/p - 1/Z * Z1
+        beta_T = []
+
+
+        for i in [0,1]:
+            vr = self.get_reduced_volume(Tr, pr, fluids[i])
+            dv_dp = 1./self.get_pdiff_pr_Vr_Tr(Tr, vr, fluids[i])
+            beta_T.append(-dv_dp/vr / self.p_crit)
+
+        beta1 = (beta_T[1] - beta_T[0])/self.acentric_r
+        return beta_T[0] + self.acentric*beta1
+
+
+    def get_departure_cp_aux(self, Tr, pr, fluid):
+
+        vr = self.get_reduced_volume(Tr, pr, fluid)
+
+        cv = self.get_departure_cv_aux(Tr, pr, fluid, Vr=vr)
+
+        dp_dT = self.get_pdiff_pr_Tr_Vr(Tr, vr, fluid)
+        dp_dv = self.get_pdiff_pr_Vr_Tr(Tr, vr, fluid)
+
+        #print(dp_dv)
+        #print(dp_dT)
+
+        cp = cv - 1 - Tr * dp_dT**2 / dp_dv
+
+        return cp
+
+    def get_departure_cp(self, T, p):
+        """
+        Get the departure (difference between real gas and ideal gas) for isobaric specific heat capacity (C_p) [J/mol/K]
+
+        Parameters
+        ----------
+        T    - Temperature [K]
+        p    - Pressure [Pa]
+
+        Returns
+        -------
+        Departure for isobaric specific heat capacity [J/mol/K]
+        """
+
+        Tr = T/self.T_crit
+        pr = p/self.p_crit
+
+        cp_0 = self.get_departure_cp_aux(Tr, pr, 'simple')
+        cp_r = self.get_departure_cp_aux(Tr, pr, 'reference')
+
+        cp_1 = (cp_r - cp_0)/self.acentric_r
+
+        #print('cp_0 - cp* =', cp_0)
+        #print('cp_1 - cp* =', cp_1)
+
+        return self.R * (cp_0 + self.acentric*cp_1)
+
+
+        # vr = self.get_reduced_volume(Tr, pr, 'simple')
+        # dp_dT_0 = self.get_pdiff_pr_Tr_Vr(Tr, vr, 'simple')
+        # dp_dv_0 = self.get_pdiff_pr_Vr_Tr(Tr, vr, 'simple')
+        # cv_0 = self.get_departure_cv_aux(Tr, pr, 'simple', Vr=vr)
+
+        # vr = self.get_reduced_volume(Tr, pr, 'reference')
+        # dp_dT_r = self.get_pdiff_pr_Tr_Vr(Tr, vr, 'reference')
+        # dp_dv_r = self.get_pdiff_pr_Vr_Tr(Tr, vr, 'reference')
+        # cv_r = self.get_departure_cv_aux(Tr, pr, 'reference', Vr=vr)
+
+        # #dp_dT = dp_dT_0 + self.acentric/self.acentric_r*(dp_dT_r - dp_dT_0)
+        # #dp_dv = dp_dv_0 + self.acentric/self.acentric_r*(dp_dv_r - dp_dv_0)
+        # #cv = cv_0 + self.acentric/self.acentric_r*(cv_r - cv_0)
+
+        # cp_0 = cv_0 - 1 - Tr*dp_dT_0**2/dp_dv_0
+        # cp_r = cv_r - 1 - Tr*dp_dT_r**2/dp_dv_r
+
+        # cp_1 = (cp_r - cp_0)/self.acentric_r
+
+        # print('cp_0 - cp* =', cp_0)
+        # print('cp_1 - cp* =', cp_1)
+
+        # cp = self.R*(cp_0 + self.acentric*cp_1)
+
+        # return cp